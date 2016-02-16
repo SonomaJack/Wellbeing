0562d94d4281a01c1ddc27417dc14bd654dca6e2
Jack Bretcher <jack@bretcher.com>
2016-02-16 13:11:14 -0500

    revert expir date to check where error is being thrown

--- functions.php
+++ functions.php
@@ -208,7 +208,7 @@ function makeSubscription($subscriptionName, $subscriptionNotes,$FullName,$Start
     $zSubscription->quantity = 1;
     
    
-    $zSubscription->TermStartDate=$StartDate__c.format('Y-m-d\TH:i:s');
+    $zSubscription->TermStartDate=$StartDate__c.date_format('Y-m-d\TH:i:s');
 		$zSubscription->Status = 'Active';
 		$zSubscription->Currency = 'USD';
 		$zSubscription->AutoRenew = 0;
