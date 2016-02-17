83736cdfeeb0073d948965096db56ffc2834dbab
Jack Bretcher <jack@bretcher.com>
2016-02-17 16:44:20 -0500

    added back the switch to ssl

--- signup/Login.php
+++ signup/Login.php
@@ -1,10 +1,10 @@
 <?php
 
-//if($_SERVER["HTTPS"] != "on")
-//{
-//    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-//    exit();
-//}
+if($_SERVER["HTTPS"] != "on")
+{
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+    exit();
+}
 
 
 	require_once '../lib/API.php';
