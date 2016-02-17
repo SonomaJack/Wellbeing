ca8ba80afc868b85cbfce935e4f8f309adcaa9ef
Jack Bretcher <jack@bretcher.com>
2016-02-17 13:43:42 -0500

    check dates before attempting to run the charge

--- signup/signup.php
+++ signup/signup.php
@@ -421,7 +421,9 @@ if ($instance->login($username, $password)) {
 	                                        // GENERATE & QUERY & POST INVOICE
 	                                        
 	                                        $accountId = $result2->result->Id;
-	                                        if ($accountId) {
+                                            $lStartDate = new DateTime($StartDate);
+                                            $formattedDate = date_format($lStartDate ,'Y-m-d\TH:i:s');
+	                                        if ($accountId && date('Y-m-d\TH:i:s') >= $formattedDate) {
 	                                            $invoiceDate  = date('Y-m-d\TH:i:s');
 	                                            $targetDate   = date('Y-m-d\TH:i:s');
 	                                            $result       = generateInvoice($instance, $accountId, $invoiceDate, $targetDate);
